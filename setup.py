c39812dd47cc2befe05243ddcdef635c787b4329
Hobson Lane <hobsonlane@gmail.com>
2015-03-26 15:52:45 -0700

    pybrain without numpy

--- setup.py
+++ setup.py
@@ -49,13 +49,16 @@ install_requires = [
     # 'pypandoc==0.8.2', 'future==0.14.3',
     'pyzmq==14.5.0', 'Unidecode==0.04.16', 'cffi==0.8.6', 'chardet==2.3.0', 'pyOpenSSL==0.14',
     'pytz==2014.10', 'python-dateutil==2.4.0',
+    # 'cython==0.22', 
     'pandas==0.15.2',
     'xlrd==0.9.3', 'Pillow==2.7', 
     'fuzzywuzzy==0.5.0', 'python-Levenshtein==0.12.0', 'progressbar2==2.7.3', 'python-slugify==0.1.0',
     'matplotlib==1.4.3',
-    'numpy==1.9.2', 'pyparsing==2.0.3', 'scipy==0.15.1',
+    #'numpy==1.9.2',
+    # 'pyparsing==2.0.3', 
+    # 'scipy==0.15.1',
     'pybrain',
-    'pug-nlp>={}'.format(version), 
+    'pug-nlp', # .{}'.format(version), 
     ]
 dependency_links = []
 # try:
@@ -81,7 +84,7 @@ EXCLUDE_FROM_PACKAGES = []
 setup(
     name=project_name,
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
-    namespace_packages=[__namespace_package__],
+    namespace_packages=[__namespace_package__, package_name, package_name + '.data', package_name + '.util'],
 
     # install non-.py files listed in MANIFEST.in (.js, .html, .txt, .md, etc)
     include_package_data = True,
