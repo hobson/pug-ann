7f7e9157dc8b8c4ea60acb17dcc03d7823f4c614
Hobson Lane <hobsonlane@gmail.com>
2015-03-26 10:15:37 -0700

    tests + travis + requirements

---
+++ pug/ann/tests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Uses the unittest module to test this app with `manage.py test`.
+"""
+
+# from django.test import TestCase
+from unittest import TestCase, main
+import doctest
+from pug.data import weather
+from pug.ann import util
+
+
+class ANNDocTest(TestCase):
+
+    def test_util(self, module=util):
+        failure_count, test_count = doctest.testmod(module, raise_on_error=False, verbose=True)
+        msg = "Ran {0} tests in {3} and {1} passed ({2} failed)".format(test_count, test_count-failure_count, failure_count, module.__file__)
+        print msg
+        if failure_count:
+            # print "Ignoring {0} doctest failures...".format(__file__)
+            self.fail(msg)
+        # return failure_count, test_count
+
+    def test_weather(self):
+        self.test_util(weather)
+
+if __name__ == '__main__':
+    main()
