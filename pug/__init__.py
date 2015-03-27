6e569ff2c92ee8e654328ae3f30d4938489baad8
Hobson Lane <hobsonlane@gmail.com>
2015-03-27 07:50:56 -0700

    try import util, data within pug.ann

--- pug/__init__.py
+++ pug/__init__.py
@@ -1,4 +1,5 @@
 from pkg_resources import declare_namespace
 declare_namespace(__name__)
 
+import ann
 __all__ = ['ann']
--- pug/ann/__init__.py
+++ pug/ann/__init__.py
@@ -1,1 +1,3 @@
+import util
+import data
 __all__ = ['util', 'net', 'data']
