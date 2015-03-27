e4123eb7b117ef8647e2f29ab65e131753aa026a
Hobson Lane <hobsonlane@gmail.com>
2015-03-27 08:00:21 -0700

    list installed packages within failing tests before failed import

--- pug/ann/tests.py
+++ pug/ann/tests.py
@@ -6,6 +6,11 @@ Uses the unittest module to test this app with `manage.py test`.
 # from django.test import TestCase
 from unittest import TestCase, main
 import doctest
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+print('Avaialble packages for import!!!!!!!!!')
+print(installed_packages_list)
 from pug.ann import util
 from pug.ann.data import weather
 
@@ -13,6 +18,7 @@ from pug.ann.data import weather
 class ANNDocTest(TestCase):
 
     def test_util(self, module=util):
+
         failure_count, test_count = doctest.testmod(module, raise_on_error=False, verbose=True)
         msg = "Ran {0} tests in {3} and {1} passed ({2} failed)".format(test_count, test_count-failure_count, failure_count, module.__file__)
         print msg
