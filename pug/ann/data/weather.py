cde3b9ad811dcba4433943f2d89052d32f92c392
Hobson Lane <hobsonlane@gmail.com>
2015-03-31 15:10:19 -0700

    live dangerously and activate a couple tests of `hourly`

--- pug/ann/data/weather.py
+++ pug/ann/data/weather.py
@@ -19,12 +19,15 @@ def hourly(location='Fresno, CA', days=1, start=None, end=None, years=1, verbosi
     The Weather Underground URL for Sacramento, CA is:
     http://www.wunderground.com/history/airport/KFCI/2011/1/1/DailyHistory.html?MR=1&format=1
 
-    >> df = hourly('Fresno, CA', verbosity=0)
+    >>> df = hourly('Fresno, CA', verbosity=-1)
+
     >> df.columns
     Index([u'TimeEDT', u'TemperatureF', u'Dew PointF', u'Humidity', u'Sea Level PressureIn', u'VisibilityMPH', u'Wind Direction', u'Wind SpeedMPH', u'Gust SpeedMPH', u'PrecipitationIn', u'Events', u'Conditions', u'WindDirDegrees', u'DateUTC'], dtype='object')
     >> 1 <= len(df) <= 24 * 2
     True
-    >> df = hourly('Fresno, CA', days=5, verbosity=0)
+
+    >>> df = hourly('Fresno, CA', days=5, verbosity=-1)
+
     >> 24 * 4 <= len(df) <= 24 * 5 * 2
     True
     """
