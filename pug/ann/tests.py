d4ddd220402b147e4eba3bb2cc3377e1ca562952
Hobson Lane <hobsonlane@gmail.com>
2015-04-04 22:10:47 -0700

    needed to do find_packages in pug-ann for local dev install to work (pip install -e or python setup.py .)

--- pug/ann/tests.py
+++ pug/ann/tests.py
@@ -9,8 +9,8 @@ import doctest
 import pip
 installed_packages = pip.get_installed_distributions()
 installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
-# print('Available packages for import!!!!!!!!!')
-# print(installed_packages_list)
+#print('Available packages for import!!!!!!!!!')
+print(installed_packages_list)
 from pug.ann import util
 from pug.ann.data import weather
 from pug.ann import example
